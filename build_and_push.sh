093c275065a85d407506a1d3d06416176fd6d057
FuChen <fuchen@foxmail.com>
2025-07-18 01:40:11 +0800

    update readme

--- build_and_push.sh
+++ build_and_push.sh
@@ -8,7 +8,11 @@ DOCKER_ARCH=${DOCKER_ARCH:-"linux/amd64"}
 
 ACCOUNT=${ACCOUNT:-$(aws sts get-caller-identity --query Account --output text)}
 REGION=${REGION:-$(aws configure get region)}
-REGION=${REGION:-$(aws ec2 describe-availability-zones --output text --query 'AvailabilityZones[0].RegionName')}
+if [ -z "$REGION" ]; then
+    TOKEN=`curl -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600"`
+    REGION=`curl -H "X-aws-ec2-metadata-token: $TOKEN" http://169.254.169.254/latest/meta-data/placement/region`
+fi
+echo $REGION
 
 # If the repository doesn't exist in ECR, create it.
 aws ecr describe-repositories --region ${REGION} --repository-names "${ECR_NAMESPACE}" > /dev/null 2>&1
